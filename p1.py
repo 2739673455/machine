d87ce837676eb88bdf10c05f07a36532ecf5dbee
pc <jkbengfeng@outlook.com>
2023-03-10 14:46:40 +0800

    v0.02

--- p1.py
+++ p1.py
@@ -3,28 +3,27 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from mpl_toolkits.mplot3d import Axes3D
 
-def crank_rocker(p1,p2,p4,l23,l34,clockwise=1): #曲柄摇杆
-    a=[1,0,1]
-    l14=np.sqrt(sum((p1-p4)**2))
-    l24=np.sqrt(np.sum((p2-p4)**2,axis=1))
-    theta041=np.arccos((p1[0]-p4[0])/l14)*(p1[1]-p4[1])/abs(p1[1]-p4[1])
-    theta241=np.arcsin(np.cross(p2-p4,p1-p4)/l14/l24)
-    theta342=np.arccos((l34**2+l24**2-l23**2)/2/l34/l24)*clockwise
-    theta043=(theta041-theta342-theta241-2*np.pi*a[clockwise]).reshape(-1,1)
-    p3=p4+l34*np.hstack((np.cos(theta043),np.sin(theta043)))
-    return p3,theta043
+def solve_theta(p): #求向量与横轴正方向夹角 2维
+    a = np.array([1,0])
+    try:
+        l = np.sqrt(np.sum((p)**2,axis=1))
+        theta = np.arccos(np.dot(p,a)/l)*(p[:,1])/abs(p[:,1])
+    except:
+        l = np.sqrt(sum((p)**2))
+        theta = np.arccos(np.dot(p,a)/l)*(p[1])/abs(p[1])
+    return theta
 
-def crank_slider(p0,p2,theta0,l23,l34,axis=1): #曲柄滑块
-    s = np.array([np.cos(theta0),np.sin(theta0)])
-    p2_ = np.delete(p2,axis,axis=1)
-    p0_ = np.delete(p0,axis)
-    d = np.cross(p2_-p0_,s)-l34
-    theta023 = theta0-np.pi-np.arcsin(d/l23).reshape([-1,1])
-    p3 = p2_+l23*np.hstack([np.cos(theta023),np.sin(theta023)])
-    p3 =np.insert(p3,axis,p0[axis],axis=1)
-    return p3
+def p_rl(p0,l,theta,*args): #2维;3维坐标以args[0]轴方向为旋转中心,arg[0]:x=0,y=1,z=2
+    p1 = np.hstack([np.cos(theta),np.sin(theta)])
+    if args==():
+        return p0+l*p1
+    try:
+        p1 = np.insert(p1,args[0],0,axis=1)
+    except:
+        p1 = np.insert(p1,args[0],0)
+    return p0+l*p1
 
-def p_rp(p0,p1,theta,axis=1): #三维坐标绕过p0点且垂直于坐标轴的直线旋转,axis:x=0,y=1,z=2
+def p_rp(p0,p1,theta,axis=1): #3维坐标绕过p0点且垂直于坐标轴的直线旋转,axis:x=0,y=1,z=2
     r0 = np.hstack([np.cos(theta),-np.sin(theta)])
     r0 = np.insert(r0,axis,0,axis=1)
     r1 = np.hstack([np.sin(theta),np.cos(theta)])
@@ -35,14 +34,35 @@ def p_rp(p0,p1,theta,axis=1): #三维坐标绕过p0点且垂直于坐标轴的�
     p2 = np.insert(p2,axis,p1[axis],axis=1)+p0
     return p2
 
-def p_rl(p0,l,theta,axis=1):
-    p1 = np.hstack([np.cos(theta),np.sin(theta)])
-    try:
-        p1 = np.insert(p1,axis,0,axis=1)
-    except:
-        p1 = np.insert(p1,axis,0)
-    p1 = p0+l*p1
-    return p1
+def crank_rocker(p2,p4,l23,l34,clockwise=1): #曲柄摇杆 2维  clockwise:134顺时针=1，134逆时针=-1
+    l24 = np.sqrt(np.sum((p2-p4)**2,axis=1))
+    theta042 = solve_theta(p2-p4)
+    theta243 = np.arccos((l34**2+l24**2-l23**2)/2/l34/l24)*(-clockwise)
+    theta043 = (theta042+theta243).reshape([-1,1])
+    p3 = p_rl(p4,l34,theta043)
+    return p3,theta043
+
+def rrp(p0,p2,theta0,l23,l34,axis=1): #曲柄滑块 3维
+    s = np.array([np.cos(theta0),np.sin(theta0)])
+    p2_ = np.delete(p2,axis,axis=1)
+    p0_ = np.delete(p0,axis)
+    d = np.cross(p2_-p0_,s)-l34
+    theta023 = theta0-np.pi-np.arcsin(d/l23).reshape([-1,1])
+    p3 = p2_+l23*np.hstack([np.cos(theta023),np.sin(theta023)])
+    p3 =np.insert(p3,axis,p0[axis],axis=1)
+    return p3
+
+def rpr(p2,p4,l23,clockwise=1): #曲柄滑块 2维 clockwise:234顺时针=1，234逆时针=-1
+    theta042 = solve_theta(p2-p4)
+    theta024 = theta042-np.pi
+    l24 = np.sqrt(np.sum((p2-p4)**2,axis=1))
+    theta423 = np.arccos(l23/l24)*clockwise
+    theta023 = (theta024+theta423).reshape([-1,1])
+    theta243 = theta423+np.pi/2*(-clockwise)
+    theta043 = theta042+theta243
+    p3 = p_rl(p2,l23,theta023)
+    return p3,theta043
+
 
 du = 180/np.pi
 hd = np.pi/180
@@ -102,22 +122,22 @@ l1_ab_xz = np.sqrt(l1_ab**2-(p1_b[:,1]-p1_a[1])**2)
 l1_bc1_xz = np.sqrt(l1_bc1**2-p1_b[:,1]**2)
 
 l1_fg1_xz = np.sqrt(l1_fg1**2-p1_f[:,1]**2)
-p1_g1,theta1_0g2g1 = crank_rocker(p1_e[[0,2]],p1_f[:,[0,2]],p1_g2[[0,2]],l1_fg1_xz,l1_g1g2)
+p1_g1,theta1_0g2g1 = crank_rocker(p1_f[:,[0,2]],p1_g2[[0,2]],l1_fg1_xz,l1_g1g2)
 p1_g1 = np.insert(p1_g1,1,p1_g2[1],axis=1)
-p1_g4 = p_rl(np.array([48.11877609,0,-0.35674442]),l1_g3g4,np.pi+theta1_g2g3g4,axis=1)
+p1_g4 = p_rl(np.array([48.11877609,0,-0.35674442]),l1_g3g4,np.pi+theta1_g2g3g4,1)
 p1_g4 = p_rp(p1_g2,p1_g4,theta1_0g2g1,axis=1)
 
-p1_h1,theta1_0h2h1 = crank_rocker(p1_g2[[0,2]],p1_g4[:,[0,2]],p1_h2[[0,2]],l1_g4h1,l1_h1h2)
+p1_h1,theta1_0h2h1 = crank_rocker(p1_g4[:,[0,2]],p1_h2[[0,2]],l1_g4h1,l1_h1h2)
 p1_h1 = np.insert(p1_h1,1,p1_g2[1],axis=1)
 theta1_0h2h3 = theta1_0h2h1+theta1_h1h2h3
 p1_h3 = p_rp(p1_h2,np.array([3.97,0,0])+p1_h2,theta1_0h2h3,axis=1)
 p1_h5 = p_rp(p1_h2,np.array([0,-4.99,21.5])+p1_h2,theta1_0h2h3,axis=1)
 
-p1_k1 = p_rl(p1_j,l1_jk1,theta1_0jk1,axis=1)
+p1_k1 = p_rl(p1_j,l1_jk1,theta1_0jk1,1)
 l1_k1h2 = np.sqrt((p1_k1[2]-p1_h2[2])**2+(p1_k1[0]-p1_h2[0])**2)
 theta1_0h2k2 = np.arccos((p1_k1[0]-p1_h2[0])/l1_k1h2)+theta1_k1h2k2
-p1_k2 = p_rl(p1_h2_1,l1_h2k2,theta1_0h2k2,axis=1)
-p1_o = p_rl(p1_n,l1_no,theta1_0no,axis=1)
+p1_k2 = p_rl(p1_h2_1,l1_h2k2,theta1_0h2k2,1)
+p1_o = p_rl(p1_n,l1_no,theta1_0no,1)
 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
